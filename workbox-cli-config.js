65ba96af33e1dee6e3489b470d4192c4d46e8dfa
Guillermo Tamanaha <guillermo.tamanaha@outlook.com>
2017-09-01 12:18:01 -0300

    service-worker

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": ".\\",
+  "globPatterns": [
+    "**/*.{mp3,css,json,js,html}"
+  ],
+  "swDest": "sw.js",
+  "globIgnores": [
+    "workbox-cli-config.js"
+  ]
+};
